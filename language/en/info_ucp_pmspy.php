2903bc9809891a85744fcf5f578b0c36dfe649e9
Forumhulp.com <info@forumhulp.com>
2018-04-08 08:00:45 +0200

    Language trouble

---
+++ language/en/info_ucp_pmspy.php
@@ -0,0 +1,22 @@
+<?php
+/**
+*
+* @package PM SPy
+* @copyright (c) 2016 ForumHulp.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'UCP_PM_SPY_INFO' => 'PM\'s can be read and or deletet by a administrator</p>',
+));
